c99dbaf0ff74d9cdb4b209bb6c901345ee339709
TINCHLIK <tinchlikmarkazi@gmail.com>
2022-01-18 09:26:27 +0500

    sodda moslashuvchan landing page

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne={
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Premium Bank',
+    headline:'Unlimited Transactions with zero fees',
+    description:"Get access to our exclusive app that allows you to send Unlimited transactions without getting ChannelMergerNode any fees",
+    buttonLabel:'Get started',
+    imgStart:false,
+    img:require('../../images/svg1.svg').default,
+    alt:'Car',
+    dark:true,
+    primary:true,
+    darkText:false
+};
+
+export const homeObjTwo={
+    id:'discover',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine:'Unlimited Access',
+    headline:'Unlimited Transactions with zero fees',
+    description:"Get access to our exclusive app that allows you to send Unlimited transactions without getting ChannelMergerNode any fees",
+    buttonLabel:'Learn More',
+    imgStart:true,
+    img:require('../../images/svg2.svg').default,
+    alt:'resume',
+    dark:false,
+    primary:false,
+    darkText:true,
+};
+
+export const homeObjThree={
+    id:'signup',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine:'Premium Bank',
+    headline:'Unlimited Transactions with zero fees',
+    description:"Get access to our exclusive app that allows you to send Unlimited transactions without getting ChannelMergerNode any fees",
+    buttonLabel:'Start Now',
+    imgStart:false,
+    img:require('../../images/svg3.svg').default,
+    alt:'texnology',
+    dark:false,
+    primary:false,
+    darkText:true
+};
+
